e243a103a2e0f357e63010c484964600c0a217cc
comtel <comtel2000@googlemail.com>
2013-11-04 17:37:18 +0100

    change to service task

--- fritz-connect/src/main/java/org/comtel/fritz/connect/MainController.java
+++ fritz-connect/src/main/java/org/comtel/fritz/connect/MainController.java
@@ -2,9 +2,11 @@ package org.comtel.fritz.connect;
 
 import java.io.IOException;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Collection;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -15,7 +17,9 @@ import javafx.animation.Timeline;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener;
+import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -71,17 +75,17 @@ public class MainController implements Initializable {
 			settingsPanel = FXMLLoader.load(MainController.class.getResource("settings.fxml"), bundle);
 			ScrollPane scrollPane = new ScrollPane(switcherPanel = new VBox());
 			switcherPanel.setId("switcherPane");
-			
+
 			splitPane.getItems().addAll(scrollPane, settingsPanel);
 			splitPane.getDividers().get(0).setPosition(1.0);
-			
+
 		} catch (IOException e) {
 			logger.error(e.getMessage(), e);
 		}
 
-		//sessionManager.bind(settingsBtn.selectedProperty(), "settingToggle");
+		// sessionManager.bind(settingsBtn.selectedProperty(), "settingToggle");
 		sessionManager.bind(splitPane.getDividers().get(0).positionProperty(), "splitPanePosition");
-		
+
 		updateDeviceList(sessionManager.getSwitchDeviceList());
 
 		sessionManager.getSwitchDeviceList().addListener(new ListChangeListener<SwitchDevice>() {
@@ -95,7 +99,7 @@ public class MainController implements Initializable {
 
 	private void updateDeviceList(List<? extends SwitchDevice> list) {
 		switcherPanel.getChildren().clear();
-		
+
 		for (final SwitchDevice dev : list) {
 
 			final CheckBox cb = new CheckBox(String.format("%s\t[%d Wh, %.2f W]", dev.getName(), dev.getEnergy(), dev.getPower() > 0 ? (double) dev.getPower() / 1000 : 0d));
@@ -104,40 +108,48 @@ public class MainController implements Initializable {
 			cb.setSelected(dev.getState() == State.ON);
 			cb.setDisable(!dev.isPresent());
 
+			cb.setPrefWidth(350);
+
 			switcherPanel.getChildren().add(cb);
 
-			//switcherPanel.getChildren().add((Node) FXMLLoader.load(DeviceController.class.getResource("device.fxml"), bundle));
-			
+			// switcherPanel.getChildren().add((Node)
+			// FXMLLoader.load(DeviceController.class.getResource("device.fxml"),
+			// bundle));
+
 			cb.selectedProperty().addListener(new ChangeListener<Boolean>() {
 				@Override
 				public void changed(ObservableValue<? extends Boolean> observable, final Boolean oldValue, Boolean selected) {
 
-					if (cb.isDisable()){
+					if (cb.isDisable()) {
 						return;
 					}
 					dev.setState(selected ? "1" : "0");
-					cb.setDisable(true);
-					Task<?> t = new Task<Object>() {
-						@Override
-						protected Object call() throws Exception {
-							try {
-								SwitcherApp.getSwitchService().updateDeviceState(dev);
-								updateStatus(dev.toString());
-							} catch (ServiceNotSupportedException e) {
-								Dialogs.create().owner(refreshBtn.getScene().getWindow()).lightweight().title("Service not available").masthead("Firmware does not support AHA webservice. Please upgrade firmware to FRITZ!OS 05.55 or later.").showExceptionInNewWindow(e);
-								updateStatus("Service not available");
-								cb.setSelected(oldValue);
-							} catch (Exception e) {
-								updateStatus(e.getMessage());
-								cb.setSelected(oldValue);
-							} finally {
-								cb.setDisable(false);
-							}
-							return null;
-						}
 
-					};
-					t.run();
+					UpdateDeviceState service = new UpdateDeviceState(dev);
+					cb.disableProperty().bind(service.runningProperty());
+					service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+						@Override
+						public void handle(WorkerStateEvent t) {
+							updateStatus(t.getSource().getValue().toString());
+						}
+					});
+					service.setOnFailed(new EventHandler<WorkerStateEvent>() {
+						@Override
+						public void handle(WorkerStateEvent t) {
+							if (t.getSource().getException() instanceof ServiceNotSupportedException) {
+								Dialogs.create().owner(refreshBtn.getScene().getWindow()).lightweight().title("Service not available")
+										.masthead("Firmware does not support AHA webservice. Please upgrade firmware to FRITZ!OS 05.55 or later.")
+										.showExceptionInNewWindow(t.getSource().getException());
+								updateStatus("Service not available");
+							} else if (t.getSource().getException() instanceof UnknownHostException) {
+								updateStatus(String.format("Synchronized with url: %s failed. Unknown host adress", t.getSource().getException().getMessage()));
+							} else {
+								updateStatus("Error: " + t.getSource().getException().getMessage());
+							}
+							logger.error("refresh failed", t.getSource().getException());
+						}
+					});
+					service.start();
 
 				}
 			});
@@ -168,32 +180,69 @@ public class MainController implements Initializable {
 
 	@FXML
 	public void refreshAll(ActionEvent event) {
-		refreshBtn.setDisable(true);
-		Task<?> t = new Task<Object>() {
-			@Override
-			protected Object call() throws Exception {
-				try {
-					long time = System.currentTimeMillis();
-					sessionManager.getSwitchDeviceList().clear();
-					sessionManager.getSwitchDeviceList().addAll(SwitcherApp.getSwitchService().getSwitchDevices());
-					updateStatus(String.format("Synchronized with url: %s [%dms]", SwitcherApp.getSwitchService().getURL(), (System.currentTimeMillis() - time)));
-				} catch (ServiceNotSupportedException e) {
-					Dialogs.create().owner(refreshBtn.getScene().getWindow()).lightweight().title("Service not available").masthead("Firmware does not support AHA webservice. Please upgrade firmware to FRITZ!OS 05.55 or later.").showExceptionInNewWindow(e);
-					updateStatus("Service not available");
-				}  catch (Exception e) {
-					updateStatus(e.getMessage());
-				} finally {
-					refreshBtn.setDisable(false);
-				}
-				return null;
-			}
 
-		};
-		t.run();
+		final long time = System.currentTimeMillis();
+
+		DeviceListRequest service = new DeviceListRequest();
+		refreshBtn.disableProperty().bind(service.runningProperty());
+
+		service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+			@Override
+			public void handle(WorkerStateEvent t) {
+				sessionManager.getSwitchDeviceList().clear();
+				sessionManager.getSwitchDeviceList().addAll((Collection<SwitchDevice>) t.getSource().getValue());
+				updateStatus(String.format("Synchronized with url: %s [%dms]", SwitcherApp.getSwitchService().getURL(), (System.currentTimeMillis() - time)));
+			}
+		});
+
+		service.setOnFailed(new EventHandler<WorkerStateEvent>() {
+			@Override
+			public void handle(WorkerStateEvent t) {
+				if (t.getSource().getException() instanceof ServiceNotSupportedException) {
+					Dialogs.create().owner(refreshBtn.getScene().getWindow()).lightweight().title("Service not available")
+							.masthead("Firmware does not support AHA webservice. Please upgrade firmware to FRITZ!OS 05.55 or later.").showExceptionInNewWindow(t.getSource().getException());
+					updateStatus("Service not available");
+				} else if (t.getSource().getException() instanceof UnknownHostException) {
+					updateStatus(String.format("Synchronized with url: %s failed. Unknown host adress", t.getSource().getException().getMessage()));
+				} else {
+					updateStatus("Error: " + t.getSource().getException().getMessage());
+				}
+				logger.error("refresh failed", t.getSource().getException());
+			}
+		});
+
+		service.start();
 
 	}
-	
-	private void updateStatus(String msg){
+
+	private class DeviceListRequest extends Service<Collection<SwitchDevice>> {
+		protected Task<Collection<SwitchDevice>> createTask() {
+			return new Task<Collection<SwitchDevice>>() {
+				protected Collection<SwitchDevice> call() throws ServiceNotSupportedException, Exception {
+					return SwitcherApp.getSwitchService().getSwitchDevices();
+				}
+			};
+		}
+	}
+
+	private class UpdateDeviceState extends Service<SwitchDevice> {
+		private final SwitchDevice dev;
+
+		public UpdateDeviceState(SwitchDevice d) {
+			dev = d;
+		}
+
+		protected Task<SwitchDevice> createTask() {
+			return new Task<SwitchDevice>() {
+				protected SwitchDevice call() throws ServiceNotSupportedException, Exception {
+					SwitcherApp.getSwitchService().updateDeviceState(dev);
+					return dev;
+				}
+			};
+		}
+	}
+
+	private void updateStatus(String msg) {
 		DateTimeFormatter df = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
 		status.setText(String.format("[%s] %s", df.format(LocalDateTime.now()), msg));
 	}
